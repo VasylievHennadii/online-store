fca1447d4e594c2ec8fd19cd62eb4d57aaa9f605
VasylievHennadii <vga.kha@gmail.com>
2019-07-09 21:24:34 +0300

    SectionProducts-slider TrendingProducts-carousel

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.js_tabs-link').on('click', function(e){
+        e.preventDefault();
+        var tabNum = $(this).attr('href');
+        $(this).addClass('tab-active');
+        $(this).parent().siblings().find('.js_tabs-link').removeClass('tab-active');
+        $(tabNum).show(400);
+        $(tabNum).addClass('.tab-active');
+        $(tabNum).siblings().hide(400);
+        $(tabNum).siblings().removeClass('.tab-active');
+    });
+});
+
+$('.container_slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    appendArrows: $('.trending_products-nav'),
+    prevArrow: '<button class="testimonials_arrow testimonials_prev"><i class="icon-angle-down"></i></button>',
+    nextArrow: '<button class="testimonials_arrow testimonials_next"><i class="icon-angle-down"></i></button>'
+  });
